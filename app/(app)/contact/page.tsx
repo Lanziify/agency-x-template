15c7e45f533cce0bfc6f2aedda1a111c671174b7
Lanziify <papasin.lancel@gmail.com>
2026-03-06 22:17:06 +0800

    chore: contact form cleanup

--- app/(app)/contact/page.tsx
+++ app/(app)/contact/page.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import Link from 'next/link';
-import { RichText } from '@payloadcms/richtext-lexical/react';
-import { BlockRenderer } from '@components/shared/blocks';
 import RecaptchaProvider from '@components/shared/providers/recaptcha-provider';
-import { Button } from '@components/ui/button';
 import { SonnerProvider } from '@components/ui/sonner-provider';
+import { ContactForm } from '@features/contact/components/form';
 import { payload } from '@lib/payload';
-import { Contact } from '@config/payload.types';
 
 export default async function ContactPage() {
   const result = await payload.find({
@@ -19,34 +15,10 @@ export default async function ContactPage() {
     depth: 1,
   });
 
-  const blockResult = result.docs[0];
-
   return (
     <React.Fragment>
       <RecaptchaProvider>
-        <div className="space-y-4 w-fit mx-auto py-10">
-          {blockResult ? (
-            <div className="border p-6 rounded-3xl">
-              {blockResult.introduction && <RichText data={blockResult.introduction} className="prose lg:prose-md mb-4" />}
-              <BlockRenderer blocks={blockResult.form as Extract<Contact, 'form'>} />
-            </div>
-          ) : (
-            <div className="prose lg:prose-md">
-              <div className="border p-4 rounded-md">
-                <h1>Contact Us</h1>
-                <p>
-                  Our contact form is currently being set up. Please check back soon or reach out to us through our available contact
-                  channels in the meantime.
-                </p>
-                <Button asChild>
-                  <Link href="/" className="no-underline">
-                    Return
-                  </Link>
-                </Button>
-              </div>
-            </div>
-          )}
-        </div>
+        <ContactForm activeFormCollection={result.docs[0]} />
       </RecaptchaProvider>
       <SonnerProvider position="top-center" />
     </React.Fragment>
--- features/contact/components/form.tsx
+++ features/contact/components/form.tsx
@@ -1,113 +1,34 @@
-'use client';
-
-import { zodResolver } from '@hookform/resolvers/zod';
-import { toast } from 'sonner';
-import { z } from 'zod';
-import { Controller, useForm } from 'react-hook-form';
+import React from 'react';
+import Link from 'next/link';
+import { RichText } from '@payloadcms/richtext-lexical/react';
+import { BlockRenderer } from '@components/shared/blocks';
 import { Button } from '@components/ui/button';
-import { Checkbox } from '@components/ui/checkbox';
-import { Field, FieldDescription, FieldError, FieldLabel } from '@components/ui/field';
-import { Form } from '@components/ui/form';
-import { Input } from '@components/ui/input';
-import { Textarea } from '@components/ui/textarea';
-import { safeCatch } from '@lib/safeCatch';
-import { formSchema } from '../schemas/contact';
+import { Contact } from '@config/payload.types';
 
-export default function ContacForm() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      privacy_policy: false,
-    },
-  });
-
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    toast.promise(
-      async () => {
-        const { data, error } = await safeCatch(async () => {
-          const response = await fetch('/api/contact', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(values),
-          });
-
-          const result = await response.json();
-
-          return await result;
-        });
-
-        if (error) {
-          throw error;
-        }
-        return data;
-      },
-      {
-        loading: <strong>Submitting form...</strong>,
-        success: (data) => ({
-          message: <strong>Your email has been sent! 🎉</strong>,
-          description: <div>{data.message}</div>,
-        }),
-        error: (error) => ({
-          message: <strong>An error has occurred</strong>,
-          description: <div>Error: {JSON.stringify(error)}</div>,
-        }),
-      }
-    );
-  }
-
+export const ContactForm: React.FC<{ activeFormCollection: Contact }> = ({ activeFormCollection }) => {
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2 max-w-3xl mx-auto py-10">
-        <Field>
-          <FieldLabel htmlFor="name" required>
-            Name
-          </FieldLabel>
-          <Input id="name" placeholder="John Doe" {...form.register('name')} />
-          <FieldError>{form.formState.errors.name?.message}</FieldError>
-        </Field>
-        <Field>
-          <FieldLabel htmlFor="email" required>
-            Email
-          </FieldLabel>
-          <Input id="email" placeholder="yourname@example.com" {...form.register('email')} />
-
-          <FieldError>{form.formState.errors.email?.message}</FieldError>
-        </Field>
-        <Field>
-          <FieldLabel htmlFor="message" required>
-            message
-          </FieldLabel>
-          <Textarea id="message" placeholder="Let us know what's on your mind." {...form.register('message')} />
-
-          <FieldError>{form.formState.errors.message?.message}</FieldError>
-        </Field>
-        <Field className="mb-4 flex flex-row items-start space-x-3 space-y-0 py-4">
-          <Controller
-            name="privacy_policy"
-            control={form.control}
-            render={({ field }) => (
-              <Checkbox
-                id="privacy_policy"
-                checked={field.value}
-                onCheckedChange={field.onChange}
-                className="flex-0 aspect-square my-1 mx-0"
-              />
-            )}
-          />
-          <div className="space-y-1 leading-none">
-            <FieldLabel htmlFor="privacy_policy" required>
-              I agree to the terms
-            </FieldLabel>
-            <FieldDescription>
-              We respect your privacy. Any information you share with us is used only to improve your experience and is kept safe.
-            </FieldDescription>
-            <FieldError>{form.formState.errors.privacy_policy?.message}</FieldError>
+    <div className="space-y-4 w-fit mx-auto py-10">
+      {activeFormCollection ? (
+        <div className="border p-6 rounded-3xl">
+          {activeFormCollection.introduction && <RichText data={activeFormCollection.introduction} className="prose lg:prose-md mb-4" />}
+          <BlockRenderer blocks={activeFormCollection.form as Extract<Contact, 'form'>} />
+        </div>
+      ) : (
+        <div className="prose lg:prose-md">
+          <div className="border p-4 rounded-md">
+            <h1>Contact Us</h1>
+            <p>
+              Our contact form is currently being set up. Please check back soon or reach out to us through our available contact channels
+              in the meantime.
+            </p>
+            <Button asChild>
+              <Link href="/" className="no-underline">
+                Return
+              </Link>
+            </Button>
           </div>
-        </Field>
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+        </div>
+      )}
+    </div>
   );
-}
+};
